198935ee9550107f76fa7394b82a82b607b0ca5f
Yamama-ops <yamamaa084@gmail.com>
2026-05-16 20:36:29 +0300

    Create Problem08_CalculatingRectangleArea.cpp

---
+++ Excercis/Problem08_CalculatingRectangleArea.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	cout << "Enter a" << endl;
+	cin >> a;
+	cout << "Enter b" << endl;
+	cin >> b;
+	float Area = a * b;
+	cout << "The Area is " << Area << endl;
+
+}
+
