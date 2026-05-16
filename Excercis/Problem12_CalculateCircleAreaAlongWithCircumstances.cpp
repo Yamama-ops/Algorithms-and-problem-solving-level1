f2c6fe8721394ef88c28b3cab06288d3ebbb6e98
Yamama-ops <yamamaa084@gmail.com>
2026-05-16 21:23:50 +0300

    Create Problem12_CalculateCircleAreaAlongWithCircumstances.cpp

---
+++ Excercis/Problem12_CalculateCircleAreaAlongWithCircumstances.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int L;
+	const double PI = 3.14;
+	cout << "Please enter L" << endl;
+	cin >> L;
+	float Area = ((L, 2) / (4 * PI));
+	cout << "The afrea of circle along the circumstances: " << Area<<endl;
+	return 0;
+
+}
