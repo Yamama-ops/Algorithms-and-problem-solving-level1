b87e66950674dfc7c1a6cc64c3b1edd69c432c79
Yamama-ops <yamamaa084@gmail.com>
2026-05-16 21:23:44 +0300

    Create Problem11_CalculatingAreaInscribedinSquare.cpp

---
+++ Excercis/Problem11_CalculatingAreaInscribedinSquare.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A;
+	const double Pi = 3.14;
+
+	cout << "Enter A" << endl;
+	cin >> A;
+	double Area = (Pi * (A, 2)) / 4;
+	cout << "The Area is" << Area;
+}
+
