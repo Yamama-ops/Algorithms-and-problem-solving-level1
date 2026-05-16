9b11728f976303215737f7686801e5b9c12bc302
Yamama-ops <yamamaa084@gmail.com>
2026-05-16 20:55:31 +0300

    Create Problem09_CalculatingTriangleArea.cpp

---
+++ Excercis/Problem09_CalculatingTriangleArea.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	double a, h;
+	cout << "Enter a" << endl;
+	cin >> a;
+	cout << "Enter h" << endl;
+	cin >> h;
+	double Area = (0.5)*(a * h);
+	cout << "The Area of triangle is: " << Area;
+	
+}
+
+
