a9d1d9d87be19860c44f794b7ace854c0904610b
Yamama-ops <yamamaa084@gmail.com>
2026-07-07 21:30:55 +0300

    Create Problem32_UsingFunctions.cpp

---
+++ Excercis/Problem32_UsingFunctions.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+void DisplayMyCardInfo()
+{
+	cout << "Name:Yamama Ahmad" << endl;
+	cout << "Age:22 years" << endl;
+	cout << "City:Damascus" << endl;
+	cout << "Country:Syria" << endl;
+}
+
+void PrintSquareStars()
+{
+	cout << "********" << endl;
+	cout << "********" << endl;
+	cout << "********" << endl;
+	cout << "********" << endl;
+	
+}
+void ILovePlaying()
+{
+	cout << "I love programming" << endl;
+}
+
+void HStars()
+{
+	cout << "*       *" << endl;
+	cout << "*       *" << endl;
+	cout << "*********" << endl;
+	cout << "*       *" << endl;
+	cout << "*       *" << endl;
+}
+int main()
+{
+	DisplayMyCardInfo();
+	cout << endl;
+	PrintSquareStars();
+	cout << endl;
+	ILovePlaying();
+	cout << endl;
+	HStars();
+	return 0;
+}
