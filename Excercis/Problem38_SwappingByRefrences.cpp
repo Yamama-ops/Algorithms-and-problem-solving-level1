627aa7ccd781caea6be362779b942e768a2da6f7
Yamama-ops <yamamaa084@gmail.com>
2026-07-09 11:59:08 +0300

    Create Problem38_SwappingByRefrences.cpp

---
+++ Excercis/Problem38_SwappingByRefrences.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void Swapping(int& x, int& y)
+{
+	x = 1;
+	y = 2;
+	int temp = x;
+	x = y;
+	y = temp;
+	cout << "The value of x is: " << x << endl;
+	cout << "The value of y  is: " << y << endl;
+}
+int main()
+{
+	int x;
+	int y;
+	Swapping(x, y);
+
+}
